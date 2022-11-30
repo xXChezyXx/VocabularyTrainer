65090d2ea23f7f63b74706ee48e34d3d2e498574
xXChezyXx <l.leonphan@gmail.com>
2022-11-30 23:23:37 +0100

    Setup

--- src/main/java/data/FileConfiguration.java
+++ src/main/java/data/FileConfiguration.java
@@ -1,4 +1,19 @@
 package data;
 
 public class FileConfiguration {
+
+    /*
+    Tipps für Malte:
+        Um Daten speichern zu können, braucht man sogenannten Path (zum Beispiel: user.malte.development.DATA)
+
+        Dafür braucht man folgende Methoden:
+        addVocabPackage(VocabPackage vocab, String path):void (Soll auch überschreiben, wenn es schon existiert)
+        getVocabPackage(String path):VocabPackage
+        getVocabPackagePath(String path):String
+        removeVocabPackage(String path):void
+
+        setDirSave(String path):void
+        getDirSave():path
+     */
+
 }
